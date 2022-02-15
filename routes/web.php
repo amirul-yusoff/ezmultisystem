c65a4437b1bd88918adcef5294da681bc030fa00
hanif-yusoff <hanif.Ikhwan@jatitinggi.com>
2022-02-15 10:42:46 +0800

    1st commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
-
+   
 use Illuminate\Support\Facades\Route;
+   
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +26,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['middleware' => ['guest']], function() {
+// Route::group(['middleware' => ['guest']], function() {
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
     
 });
