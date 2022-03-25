eebf8d6b4cf91267c160d1a5d5d2ece6c8b84c10
amirulhafizey <amirulhafizey@gmail.com>
2022-03-25 09:16:02 +0800

    3rd commit

--- app/Http/Controllers/MyAddressController.php
+++ app/Http/Controllers/MyAddressController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\has_address;
+use App\Models\api;
 use Illuminate\Support\Facades\Auth;
 use Mail;
 
@@ -28,19 +29,6 @@ class MyAddressController extends Controller
     public function create()
     {
       $user = Auth::user();
-      // $address = 'BTM 2nd Stage, Bengaluru, Karnataka 560076'; // Address
-      // $apiKey = 'AIzaSyBnyn_vmBToXHsnDw5-7bhBzTVO1cDw6tw'; // Google maps now requires an API key.
-      // // Get JSON results from this request
-      // $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($address).'&sensor=false&key='.$apiKey);
-      // $geo = json_decode($geo, true); // Convert the JSON to an array
-
-      // if (isset($geo['status']) && ($geo['status'] == 'OK')) {
-      //   $latitude = $geo['results'][0]['geometry']['location']['lat']; // Latitude
-      //   $longitude = $geo['results'][0]['geometry']['location']['lng']; // Longitude
-      // }
-      // dd($geo, $latitude,$longitude);
-
-
       $data = has_address::where('user_id',$user->id)->where('is_deleted',0)->get();
       $url = 'my-address';
 
@@ -53,7 +41,8 @@ class MyAddressController extends Controller
       $input = $request->all();
       $address = $input['address_1'].$input['address_2'].$input['city'].','.$input['state'].','.$input['postcode'].','.$input['country'];
 
-      $apiKey = 'AIzaSyBnyn_vmBToXHsnDw5-7bhBzTVO1cDw6tw'; // Google maps now requires an API key.
+      $apiKey = api::where('id',1)->first();
+      $apiKey =  $apiKey->key;
       // Get JSON results from this request
       $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($address).'&sensor=false&key='.$apiKey);
       $geo = json_decode($geo, true); // Convert the JSON to an array
@@ -85,7 +74,8 @@ class MyAddressController extends Controller
 
     function getDistance($addressFrom, $addressTo, $unit = ''){
       // Google API key
-      $apiKey = 'AIzaSyBnyn_vmBToXHsnDw5-7bhBzTVO1cDw6tw';
+      $apiKey = api::where('id',1)->first();
+      $apiKey =  $apiKey->key;
       
       // Change address format
       $formattedAddrFrom    = str_replace(' ', '+', $addressFrom);
